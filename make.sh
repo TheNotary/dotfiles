cab2cda7a1081bc1e5a4aee5ada3532101ea6d7a
TheNotary <no@email.plz>
2016-07-08 20:56:29 -0700

    improves comments

--- make.sh
+++ make.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+#######################################################################
+#                           Dotfiles                                  #
+# This script creates symlinks from the home directory to any desired #
+# dotfiles in ~/dotfiles                                              #
+#######################################################################
+
+###############################
+#          Variables          #
+###############################
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
@@ -12,9 +16,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )  # Get current directory o
 files="$(ls $DIR)"                   # list of files/folders to symlink in homedir
 
 
-##########
+###############################
+#            Logic            #
+###############################
 
-# Make sure the directory's name is dotfiles... otherwise the user could be getting more than he or she barginned for
+# Make sure the directory's name is dotfiles... otherwise the user could be 
+# getting more than he or she bargained for
 if [ "$( cd $DIR && echo ${PWD##*/} )" != "dotfiles" ]
 then
   echo "The folder you ran this script from isn't named 'dotfiles'... so the script \nhas been blocked from to prevent you from accidentally creating a bunch of symlinks you don't actually want"
@@ -35,9 +42,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
   
-  #######################################
   # skip the README.md and make.sh file #
-  #######################################
   if [ "$file" = "README.md" ] || [ "$file" = "make.sh" ]
   then
    echo "skipping $file"
